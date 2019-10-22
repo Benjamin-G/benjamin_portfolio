7eaa34b3d49f89aeebdd36c365c172799f55827e
Benjamin Geyer <brgeyer49@gmail.com>
2019-10-21 22:02:49 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-laptop"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Benjamin Geyer</h1>
+                <p>
+                  About Me
+                  <br/>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices. Habitasse platea dictumst quisque sagittis purus sit amet. Sit amet dictum sit amet justo donec enim. In mollis nunc sed id semper risus in. Ipsum faucibus vitae aliquet nec ullamcorper sit. A iaculis at erat pellentesque adipiscing. Mauris cursus mattis molestie a iaculis at erat pellentesque. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Sed arcu non odio euismod. Dui nunc mattis enim ut. Viverra nibh cras pulvinar mattis nunc sed. Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. Nunc vel risus commodo viverra maecenas. Morbi tempus iaculis urna id. Varius duis at consectetur lorem donec massa. Massa enim nec dui nunc mattis enim ut tellus elementum. Lacus luctus accumsan tortor posuere ac ut consequat semper.
+                  <br/>
+                  <br/>
+                  <br/>
+                  Tech known:
+                  <ul>
+                    <li>javascript</li>
+                  </ul>
+                </p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Story</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
