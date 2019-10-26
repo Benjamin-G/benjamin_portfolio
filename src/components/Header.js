cb4fb02568abeec9754820453ffa8e18b45bf31c
Benjamin Geyer <brgeyer49@gmail.com>
2019-10-26 15:48:35 -0700

    about me

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,27 +16,28 @@ const Header = (props) => (
                   <h4>About Me</h4>
                   I am motivated software engineer from Pittsburgh, PA  currently living in beautiful San Diego, CA. I love the web platform and interesting use cases of CS concepts. Functional programming, data engineering and modern web development are some of my favorite topics.
                   <br/>
-                  <br/>
-                  <p>
-                    <a title="Benjamin Geyer - Linkedin" href="https://www.linkedin.com/in/benjamin-r-geyer/">
-                      <FontAwesomeIcon icon={['fab', 'linkedin-in']} size="3x"/>
-                    </a>
-                  </p>
-                    <h4>Hire me for</h4>
-                    <a title="Benjamin Geyer - Github" href="https://github.com/Benjamin-G"><FontAwesomeIcon icon={['fab', 'github-alt']} size="lg"/>  Github </a> <br/>
-                    <FontAwesomeIcon icon={['fab', 'docker']} size="lg"/> Docker <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'js']} size="lg"/> Javascript <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'python']} size="lg"/> Python <br/>
-                    <FontAwesomeIcon icon={['fab', 'aws']} size="lg"/> Aws <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'react']} size="lg"/> React / Redux <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'css3-alt']} size="lg"/> CSS3 <br/>
-                    <FontAwesomeIcon icon={['fab', 'google']} size="lg"/> Firebase <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'html5']} size="lg"/> HTML5 <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'linux']} size="lg"/> Command Line <br/>
-                    <FontAwesomeIcon icon={['fab', 'node']} size="lg"/> Node JS <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'sketch']} size="lg"/> Ruby <span>| </span>  
-                    <FontAwesomeIcon icon={['fab', 'npm']} size="lg"/> NPM <br/>
-                    <h5>and many more technologies</h5>
+                  <p></p>
+                  <h4>Connect with me on Linkdin</h4>
+                  <a title="Benjamin Geyer - Linkedin" href="https://www.linkedin.com/in/benjamin-r-geyer/">
+                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} size="3x"/>
+                  </a>
+                  <p></p>
+                  
+                  <h4>Hire me for</h4>
+                  <a title="Benjamin Geyer - Github" href="https://github.com/Benjamin-G"><FontAwesomeIcon icon={['fab', 'github-alt']} size="lg"/>  Github </a> <br/>
+                  <FontAwesomeIcon icon={['fab', 'docker']} size="lg"/> Docker <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'js']} size="lg"/> Javascript <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'python']} size="lg"/> Python <br/>
+                  <FontAwesomeIcon icon={['fab', 'aws']} size="lg"/> Aws <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'react']} size="lg"/> React / Redux <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'css3-alt']} size="lg"/> CSS3 <br/>
+                  <FontAwesomeIcon icon={['fab', 'google']} size="lg"/> Firebase <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'html5']} size="lg"/> HTML5 <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'linux']} size="lg"/> Command Line <br/>
+                  <FontAwesomeIcon icon={['fab', 'node']} size="lg"/> Node JS <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'sketch']} size="lg"/> Ruby <span>| </span>  
+                  <FontAwesomeIcon icon={['fab', 'npm']} size="lg"/> NPM <br/>
+                  <h5>and many more technologies</h5>
                   </p>
                   <div>
                     <p>Check out my story, see projects, or contact me below<br/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
+//import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pittsburgh from '../images/pittsburgh.jpg'
 
 class Main extends React.Component {
   render() {
@@ -28,29 +29,24 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">How did I get here?</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={pittsburgh} alt="Pittsburgh" />
           </span>
-          <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+          <p style={{textIndent: "50px" }}>
+            I was born in the late 80’s in Pittsburgh, PA.  I started with Object Oriented Programming in C++ and Java during high school.  That led me to pursue a Computer Science degree after high school then Mathematics; followed by a short career in Material Science after an Asc. Degree in Chemistry. The education was with Bidwell Training center, which is where I learned to love nontraditional forms of education.  I finished my BS Degree in Science from the University of Pittsburgh with night classes while working in a cutting edge Neuroscience Department (PIND).  This allowed me to learn more about Geology, Physics, and Neuroscience.  I did get some exposure to SQL in a GIS course. 
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+          <p style={{textIndent: "50px" }}>
+            After the end of 2016 with my degree being completed; I found Javascript, functional programming and the wonderful deep field of web development.  I quickly became aware of my passion was in software.  With my background in CS I quickly learned JS (React and Vanilla) , Webpack , Babel , HTML5/CSS3, Node and Ruby (Rails).  This was just a starting point.  I started pursuing jobs very early on, this was very challenging and valuable looking back.  Having been given coding assignments in tools I had no experience with such as Python ( Flask ), Websockets, C#( .NET ) and Java (spring).  During this time I used tools like Hackerrank and Codeingames to hone in my coding flow and problem solving.  Consistently reading Stackoverflow, Medium articles and started to follow the tech community on Twitter.  Also taking a consistent stream of Udemy course to build projects, learn concepts and learn technologies outside my wheelhouse, that is where I really honed in my React, Redux, Docker, SQL and vanilla JS, HTML, CSS.  Some great teachers I found were Bret Fisher, Stephen Grider, Andrew Mead, and Jonas Schmedtmann
+          </p>
+          <p style={{textIndent: "50px" }}>
+            Most importantly, I started to participate in the coding community in Pittsburgh.  Having gone to many meetups from Code&Supply, Pittsburgh Functional, Pittsburgh QA, Python Users and more.  I had gained insight into many of the deep corners of this field.  It cannot be overstated  how important these meetups where.  Getting exposure to Kotlin, Scala, Go, Rust, and Elixir.  Not just technologies where experienced, soft skills talks and discussion as well. 
+          </p>
+          <p style={{textIndent: "50px" }}>
+            Forward to 2018 spring, still unemployed and after a major loss in my family. I took sometime off from the job hunt.   I pursued some time in looking into Game Development and did not love it.   I did also teach myself some Data Science with Python, Jupyter Notebooks, Numpy and Plotly. I gained insight that I enjoy Data alot.  I continued to keep my web development sharp along this path.  This is the period I learned Docker, which I really enjoy. 
+          </p>
+          <p style={{textIndent: "50px" }}>
+            In the spring of 2019, I found myself inspired by the landscape and opportunities here in San Diego.  I signed up for Learn Academy Echo Class of 2019.  I quit my job, packed my bags and moved across the country .  I wanted to grow as an individual by moving to a completely new region in the USA.  Continuing to attend meetups here, I have happily found extremely talented and friendly engineers.  My favorite meetups here are SD Tech Hub, Ruby, Data Engineering, Javascript, and Tech Events.  I am also excited to learn more about F# (functional/domain design), Airflow, Sagemaker, Redshift and SQL.  Thank you for reading my story, I cannot wait to continue adding more paragraphs. 
           </p>
           {close}
         </article>
