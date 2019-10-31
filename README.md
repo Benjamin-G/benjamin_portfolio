fbaf717571b6f963fc6e30719dbf28e3e1008302
Benjamin Geyer <brgeyer49@gmail.com>
2019-10-31 09:56:16 -0700

    bryan edits!

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# gatsby-starter-dimension
+# Benjamin Geyer portfolio
 
-http://benjamingeyer.io.s3-website-us-east-1.amazonaws.com
+http://benjamingeyer.io
 
-**This is a starter for Gatsby.js V2.**
 
-**The older V1 version of this starter can be found on the v1 branch:**
-
-Gatsby.js V2 starter based on the Dimension site template, designed by HTML5 UP. Check out https://codebushi.com/gatsby-starters-and-themes/ for more Gatsby starters and templates.
-
-## Preview
-
-https://gatsby-dimension.surge.sh/
+Gatsby, Graphql and Zesty io
 
 ## Installation
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,12 +17,11 @@ const Header = (props) => (
                   I am motivated software engineer from Pittsburgh, PA  currently living in beautiful San Diego, CA. I love the web platform and interesting use cases of CS concepts. Functional programming, data engineering and modern web development are some of my favorite topics.
                   <br/>
                   <p></p>
-                  <h4>Connect with me on Linkdin</h4>
+                  <h4>Connect with me on Linkedin</h4>
                   <a title="Benjamin Geyer - Linkedin" href="https://www.linkedin.com/in/benjamin-r-geyer/">
                     <FontAwesomeIcon icon={['fab', 'linkedin-in']} size="3x"/>
                   </a>
                   <p></p>
-                  
                   <h4>Hire me for</h4>
                   <a title="Benjamin Geyer - Github" href="https://github.com/Benjamin-G"><FontAwesomeIcon icon={['fab', 'github-alt']} size="lg"/>  Github </a> <br/>
                   <FontAwesomeIcon icon={['fab', 'docker']} size="lg"/> Docker <span>| </span>  
@@ -40,7 +39,7 @@ const Header = (props) => (
                   <h5>and many more technologies</h5>
                   </p>
                   <div>
-                    <p>Check out my story, see projects, or contact me below<br/>
+                    <p>Check out my story, see projects, or contact me below and
                     Have an awesome day!</p>
                   </div>
             </div>
